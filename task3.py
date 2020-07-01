235c00622c2aef1ca450f4856b8a57058a70842a
shreyanshmohnot <shreyansh32@gmail.com>
2020-07-01 15:31:44 -0400

    Update task3.py

---
+++ task3.py
@@ -0,0 +1,17 @@
+# 1. 
+list1 = ['hello', 'world', 10, 20, 0.49, 11.99, 21.99, 11+2j, 50+100j, 'consult add']
+
+# 2.
+list2 = [1] * 5
+
+# 3.
+add = sum(list2)
+mul = 1
+for i in list2:
+    ans2 *= i
+
+# 4.
+print(min(list2))
+print(max(list2))
+
+# 5.
