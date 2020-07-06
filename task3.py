c56b69ed46e08ee663e8c8863a45276c64a18f0f
shreyanshmohnot <shreyansh32@gmail.com>
2020-07-06 18:16:44 -0400

    task3 completed

--- task3.py
+++ task3.py
@@ -8,10 +8,44 @@ list2 = [1] * 5
 add = sum(list2)
 mul = 1
 for i in list2:
-    ans2 *= i
+    mul *= i
 
 # 4.
 print(min(list2))
 print(max(list2))
 
 # 5.
+pre_def = [int(i*i/7) for i in range(1,50,5)]
+for i in pre_def:
+    if i%2 == 0:
+        pre_def.remove(i)
+
+# 6.
+sq_list = [i*i for i in range(31)]
+result1 = sq_list[:5]
+result1.extend(sq_list[-5:])
+
+# 7.
+sdata = [[1,3,5,7,9,10],[2,4,6,8]]
+sdata[0].pop()
+sdata[0].extend(sdata[1])
+print(sdata[0])
+
+# 8.
+ndict = {}
+a = {1:10, 2:20}
+b = {3:30, 4:40}
+ndict.update(a)
+ndict.update(b)
+print(ndict)
+
+# 9.
+n = 5
+result2 = {}
+for i in range(1, n+1):
+    result2[i]=i*i
+
+# 10.
+input_seq = input("Enter numbers separated by comma: ").split(',')
+print(input_seq)
+print(tuple(input_seq))
